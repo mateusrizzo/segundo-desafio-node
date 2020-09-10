255641e0de43d2dbcfb96d4190532fe9c8d0b4a5
Mateus Gonçalves Rizzo <mateusrizzo13@gmail.com>
2020-09-10 11:36:10 -0300

    challenge complete

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const typeOfTransaction = type;
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw Error("You don't have balance to do this transaction");
+    }
+    if (typeOfTransaction !== 'income' && typeOfTransaction !== 'outcome') {
+      throw Error('Invalid transaction type');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type: typeOfTransaction,
+    });
+    return transaction;
   }
 }
 
